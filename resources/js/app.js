ae35ed4a44c36ed02a2afbe31fc4aa5720f8e912
flurrywild <>
2023-05-10 18:42:04 +0400

    import

---
+++ resources/js/app.js
@@ -0,0 +1,27 @@
+import { createApp } from 'vue';
+
+import App from './components/App.vue';
+import PrimeVue from 'primevue/config';
+
+import store from './store';
+
+import Panel from 'primevue/panel';
+import Editor from 'primevue/editor';
+import Button from 'primevue/button';
+
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import 'primevue/resources/primevue.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const app = createApp(App);
+
+app.component('Panel', Panel);
+app.component('Editor', Editor);
+app.component('Button', Button);
+
+app.use(store);
+app.use(PrimeVue);
+
+app.mount("#app");
